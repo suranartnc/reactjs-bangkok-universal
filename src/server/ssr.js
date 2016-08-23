51c79ee871c1c1f49e5531db5322b0ac2c83c1e3
SuranartN <suranartnc@gmail.com>
2016-08-23 23:56:54 +0700

    add support for setting title at server-side rendering using react-helmet

--- src/server/ssr.js
+++ src/server/ssr.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { RouterContext, match } from 'react-router'
+import Helmet from 'react-helmet'
 import { Provider } from 'react-redux'
 import createStore from 'shared/store/createStore'
 import routes from 'shared/routes'
@@ -10,30 +11,33 @@ import config from '../../src/shared/configs';
 const wdsPath = `http://${config.host}:${config.wdsPort}/build/`;
 const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
 
-const renderPage = (reactComponent, initialState) => (`
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>ReactJS Bangkok - Universal Web Apps</title>
-      ${process.env.NODE_ENV === 'production' ? '<link rel="stylesheet" href="' + assetsManifest.app.css + '" />' : ''}
-    </head>
-    <body>
-      <div id="root">${reactComponent}</div>
-      <script>
-        window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
-      </script>
-      ${process.env.NODE_ENV === 'production' ?
-        `
-          <script src="${assetsManifest.vendor.js}"></script>
-          <script src="${assetsManifest.app.js}"></script>
-        `
-        : `<script src="${wdsPath + 'main.js'}"></script>`
-      }
-    </body>
-  </html>
-`)
+const renderPage = (reactComponent, initialState) => {
+  let head = Helmet.rewind()
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${head.title.toString()}
+        ${process.env.NODE_ENV === 'production' ? '<link rel="stylesheet" href="' + assetsManifest.app.css + '" />' : ''}
+      </head>
+      <body>
+        <div id="root">${reactComponent}</div>
+        <script>
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
+        </script>
+        ${process.env.NODE_ENV === 'production' ?
+          `
+            <script src="${assetsManifest.vendor.js}"></script>
+            <script src="${assetsManifest.app.js}"></script>
+          `
+          : `<script src="${wdsPath + 'main.js'}"></script>`
+        }
+      </body>
+    </html>
+  `
+}
 
 export default function(req, res) {
   const store = createStore()
